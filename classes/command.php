00ac4c62233c839f1d5b403e4c524a3142df9ade
Ivan Kerin <ikerin@gmail.com>
2011-11-09 10:45:44 +0200

    Author Ivan Kerin

--- classes/command.php
+++ classes/command.php
@@ -6,7 +6,7 @@
  * A class to be extended by all commands in the application, provides some logging methods
  *
  * @package    OpenBuildings/kohana-cli
- * @author     Ivan Kerinerin
+ * @author     Ivan Kerin
  * @copyright  (c) 2011 OpenBuildings Inc.
  * @license    http://creativecommons.org/licenses/by-sa/3.0/legalcode
  */
